49eed03fe8d101656f1ba4641caa1484351e1ba4
llastkrakw <llastkrakw21@gmail.com>
2020-07-21 04:45:46 +0100

    init

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+const indexController = require('../controllers/indexController');
 
 /* GET home page. */
 router.get('/', indexController.index);
