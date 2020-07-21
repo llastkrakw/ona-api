9e2042b9e3fee9d8d9c96a00aa601c48d75a3eac
Djouendjeu Emma <llastkrakw@iMac-de-zamo.local>
2020-07-21 13:24:09 +0100

    base class

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var indexRouter = require('./routes/router');
 var app = express();
 
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
